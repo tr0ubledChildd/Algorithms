be5c96b1fbb40263446964228f5b73dd61c46182
tr0ubledChildd <jacobv66@nycstudents.net>
2024-12-04 11:15:45 -0500

    Create Algorithms.java

---
+++ Algorithms.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Algorithms {
+    private static File f;
+    private static Scanner s;
+    public static void main(String[] args) throws FileNotFoundException {
+        f = new File("Numbers.txt");
+        int odds = odds();
+        int evens = evens();
+        System.out.println(odds);
+        s.close();
+    }
+
+    public static int odds() throws FileNotFoundException{
+        s = new Scanner(f);
+        int odds = 0;
+        while (s.hasNext()) {
+            if (s.nextInt() % 2 != 0)
+                odds++;
+        }
+        return odds;
+    }
+}
